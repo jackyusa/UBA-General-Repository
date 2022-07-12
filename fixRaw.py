a8f38b8b7814fd4e140fdb1afb28ba5690d8b179
Jacky Li <90122922+jackyusa@users.noreply.github.com>
2022-07-12 15:28:31 -0400

    Add files via upload

--- fixRaw.py
+++ fixRaw.py
@@ -10,9 +10,10 @@ response = requests.get("https://v6.exchangerate-api.com/v6/81a98818ac24839899cd
 data = response.text
 parse_json = json.loads(data)
 rates = parse_json['conversion_rates']
-print(rates['AUD'])
 
-df = pd.read_csv('rawFile.csv', index_col=0)
+###############################################################################################################################
+# Creates the dataframe for the file
+df = pd.read_csv("inputTest.csv", index_col=0)
 
 ###############################################################################################################################
 # Remove rows that are not ODA/CAA/LAA or OAB/OAP that
@@ -37,7 +38,7 @@ def removeRows():
         (~df.index.str.contains("1121",case=False))
     ].index,inplace=True)
 
-    df.to_csv('rawFile.csv')
+    df.to_csv('inputRawFile.csv')
     print("Irrelevant scheme types have been removed and account numbers containing 1121 have been saved.")
 
 ###############################################################################################################################
@@ -47,33 +48,53 @@ def emptyMaturity():
     
     df['MATURITY_DATE'].fillna(tomorrow_datetime.date(), inplace=True)
     df['MATURITY_DATE'] = pd.to_datetime(df['MATURITY_DATE'],errors='coerce')
-    df.to_csv('rawFile.csv')
+    df.to_csv('inputRawFile.csv')
 
-    print("Empty Maturity_Date cells have been changed to tomorrows date.")
+    print("Empty Maturity_Date cells have been set to tomorrow's date: "+ tomorrow_datetime)
 
 ###############################################################################################################################
 # Sorts the dataframe based on the Maturity date in ascending order
 def sortByMaturity():
     df["MATURITY_DATE"] = pd.to_datetime(df["MATURITY_DATE"])
     df1 = df.sort_values(by='MATURITY_DATE')
-    df1.to_csv('rawFile.csv')
+    df.to_csv('inputRawFile.csv')
 
     print("Maturity dates sorted sucessfully!")
 
 ###############################################################################################################################
 # Add a "AMOUNT" column after CLS_BALANCE column 
 # then multiply CLS_BALANCE cells by -1
-#def addAmount():
-    
+def addAmountColumn():
+    df.insert(4,'AMOUNT','')
+    df['AMOUNT'] = df['CLS_BALANCE'] * -1
+    df.to_csv('inputRawFile.csv')
+    print("Amount column has been inserted with negative values")
+
+def insertColumn(columnName,index,values):
+    df.insert(index,columnName,values)
+    df.to_csv('inputRawFile.csv')
+    print(columnName + " has been inserted into the dataframe in column " + index)
 
 ###############################################################################################################################
 # add USD column and covert AMOUNT to USD currency by the related conversion rate
 # add USD_AMOUNT after CURRENCY
-#def convertToUSD():
-
+def convertToUSD():
+    df.insert(9,'USD_AMOUNT','')
+    x = len(df.index)
+    for i in range(0,x):
+        df['USD_AMOUNT'].values[i] = df['CLS_BALANCE'].values[i] * rates[df['CRNCY'].values[i]]
+    df.to_csv('inputRawFile.csv')
+    print("USD_AMOUNT column has been inserted and currencies have been converted to USD")
 
 ###############################################################################################################################
 def fixFile():
     emptyMaturity()
     sortByMaturity()
     removeRows()
+
+###############################################################################################################################
+# clears the file
+def clearFile(file):
+    f = open(file, "w")
+    f.truncate()
+    f.close()
--- test.py
+++ test.py
@@ -1,24 +1,48 @@
 from cmath import nan
 import pandas as pd
 from datetime import timedelta, datetime
+import requests
+from pkg_resources import empty_provider
+import json
 
-df = pd.read_csv('test.csv',index_col=0)
+response = requests.get("https://v6.exchangerate-api.com/v6/81a98818ac24839899cdc992/latest/USD")
+data = response.text
+parse_json = json.loads(data)
+rates = parse_json['conversion_rates']
+
+df = pd.read_csv('inputTest.csv',index_col=0)
 
 # test setting empty ones to 1 then changing them
-def emptyMaturity():
+def emptyDate():
     tomorrow_datetime = datetime.now() + timedelta(days=1)
-    df['date'].fillna(tomorrow_datetime.date(), inplace=True)
-    df["date"] = pd.to_datetime(df["date"])
-    df.to_csv('test.csv')
+    df['Date'].fillna(tomorrow_datetime.date(), inplace=True)
+    df["Date"] = pd.to_datetime(df["Date"])
+    df1 = df.sort_values(by="Date")
+    df1.to_csv('inputTest.csv')
 
     print("All empty Maturity dates have been set to tomorrows date.")
 
 def removeRows():
     df.drop(df[
-        (df['amount'] == 230) &
+        (df['Amount'] == 230) &
         (df.index.str.contains("a"))
     ].index,inplace=True)
 
-    df.to_csv("test.csv")
+    df.to_csv('inputTest.csv')
 
-removeRows()
+def clearFile():
+    f = open('inputTest.csv', "w")
+    f.truncate()
+    f.close()
+
+def addColumn():
+    df.insert(2,'neg_Amount','')
+    df.to_csv('inputTest.csv')
+
+def convertToUSD():
+    df.insert(4,'USD_Amount','')
+    x = len(df.index)
+    for i in range(0,x):
+        df['USD_Amount'].values[i] = df['Amount'].values[i] * rates[df['Currency'].values[i]]
+    df.to_csv('inputTest.csv')
+convertToUSD()
