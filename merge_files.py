46972d12bfacebea9d90b4467f126995593a8841
Daniel Bueno <78511075+dbueno3@users.noreply.github.com>
2022-07-14 09:51:54 -0400

    Merging Tress to Core

--- merge_files.py
+++ merge_files.py
@@ -1,4 +1,23 @@
 import pandas as pd
 import numpy as np
 from fixRaw import *
+#Deal NO = ACCOUNT_NO
+def accountNumberMerge():
+    2
+#Counterparty Mnemonic = ACCOUNT_NAME
+def accountNameMerge():
+    2
+#SCHEME_TYPE = Subtype 
+def SchemeTypeMerge(): 
+    1 
+#Start Date = ACCT_OPEN_DATE 
+def accOpenDateMerge():
+    1
 
+#End Date = Maturity_date
+def maturityDateMerge(): 
+    2
+#CRNY = Currency 
+#Principle Amount = USD Amount 
+def usdAMountMerge(): 
+    "$"
